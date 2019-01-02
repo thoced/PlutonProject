43cfc7ddd36b528c1c6ae5633e7b950c55e6ec08
thonon <thonon_cedric@hotmail.com>
2019-01-02 13:50:56 +0100

    nouveau script sql

--- src/infos/db_pluton.sql
+++ src/infos/db_pluton.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 30, 2018 at 08:03 PM
+-- Generation Time: Jan 02, 2019 at 01:50 PM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.3
 
@@ -30,23 +30,46 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `t_dossiers` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `nom` varchar(256) NOT NULL,
+  `name` varchar(256) NOT NULL,
   `comment` text NOT NULL,
   `ref_id_sections` bigint(20) UNSIGNED NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `t_events`
+-- Table structure for table `t_identites`
 --
 
-CREATE TABLE `t_events` (
+CREATE TABLE `t_identites` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `start_time` datetime NOT NULL,
-  `end_time` datetime NOT NULL,
+  `numero` varchar(32) NOT NULL,
+  `identite` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_link_identites_observations`
+--
+
+CREATE TABLE `t_link_identites_observations` (
+  `ref_id_identites` bigint(20) UNSIGNED NOT NULL,
+  `ref_id_observations` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_observations`
+--
+
+CREATE TABLE `t_observations` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `namefile` varchar(256) NOT NULL,
+  `comment` text NOT NULL,
   `ref_id_dossiers` bigint(20) UNSIGNED NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -56,9 +79,9 @@ CREATE TABLE `t_events` (
 
 CREATE TABLE `t_sections` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(256) NOT NULL,
+  `name` varchar(64) NOT NULL,
   `comment` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -68,12 +91,12 @@ CREATE TABLE `t_sections` (
 
 CREATE TABLE `t_users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `nom` varchar(256) NOT NULL,
-  `prenom` varchar(256) NOT NULL,
+  `nom` varchar(64) NOT NULL,
+  `prenom` varchar(64) NOT NULL,
   `login` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL,
+  `password` varchar(256) NOT NULL,
   `ref_id_sections` bigint(20) UNSIGNED NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -83,13 +106,30 @@ CREATE TABLE `t_users` (
 -- Indexes for table `t_dossiers`
 --
 ALTER TABLE `t_dossiers`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `index_foreign_ref_id_sections` (`ref_id_sections`);
 
 --
--- Indexes for table `t_events`
+-- Indexes for table `t_identites`
 --
-ALTER TABLE `t_events`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `t_identites`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_couple_numero_identite` (`numero`,`identite`);
+
+--
+-- Indexes for table `t_link_identites_observations`
+--
+ALTER TABLE `t_link_identites_observations`
+  ADD UNIQUE KEY `unique_couple_ref_ident_ref_obn` (`ref_id_identites`,`ref_id_observations`),
+  ADD KEY `index_foreign_ref_id_identites` (`ref_id_identites`),
+  ADD KEY `index_foreign_ref_id_observations` (`ref_id_observations`);
+
+--
+-- Indexes for table `t_observations`
+--
+ALTER TABLE `t_observations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `index_foreign_ref_id_dossiers` (`ref_id_dossiers`);
 
 --
 -- Indexes for table `t_sections`
@@ -101,7 +141,8 @@ ALTER TABLE `t_sections`
 -- Indexes for table `t_users`
 --
 ALTER TABLE `t_users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `index_foreign_ref_id_sections` (`ref_id_sections`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -114,9 +155,15 @@ ALTER TABLE `t_dossiers`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `t_events`
+-- AUTO_INCREMENT for table `t_identites`
 --
-ALTER TABLE `t_events`
+ALTER TABLE `t_identites`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `t_observations`
+--
+ALTER TABLE `t_observations`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
@@ -130,6 +177,35 @@ ALTER TABLE `t_sections`
 --
 ALTER TABLE `t_users`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `t_dossiers`
+--
+ALTER TABLE `t_dossiers`
+  ADD CONSTRAINT `t_dossiers_ibfk_1` FOREIGN KEY (`ref_id_sections`) REFERENCES `t_sections` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `t_link_identites_observations`
+--
+ALTER TABLE `t_link_identites_observations`
+  ADD CONSTRAINT `t_link_identites_observations_ibfk_1` FOREIGN KEY (`ref_id_identites`) REFERENCES `t_identites` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `t_link_identites_observations_ibfk_2` FOREIGN KEY (`ref_id_observations`) REFERENCES `t_observations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `t_observations`
+--
+ALTER TABLE `t_observations`
+  ADD CONSTRAINT `t_observations_ibfk_1` FOREIGN KEY (`ref_id_dossiers`) REFERENCES `t_dossiers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `t_users`
+--
+ALTER TABLE `t_users`
+  ADD CONSTRAINT `t_users_ibfk_1` FOREIGN KEY (`ref_id_sections`) REFERENCES `t_sections` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
