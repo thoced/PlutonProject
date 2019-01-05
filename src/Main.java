36799933079e3da96d123df490a5a0e3e9958112
Geoffrey@Vickx.be <3529oPtion>
2019-01-05 18:12:22 +0100

    Création Sous-branche MainForm

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/MainForm.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Pluton Project");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
