7656adfe383a796c422afd78ed3819c34b9c8392
Geoffrey@Vickx.be <3529oPtion>
2019-01-09 20:29:03 +0100

    fenetre de login

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import controllers.LoginFormCtrl;
+import controllers.MainFormCtrl;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,12 +10,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("views/MainForm.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("views/MainForm.fxml"));
+        Parent root = loader.load();
+        MainFormCtrl controller = loader.getController();
+        controller.setStage(primaryStage);
         primaryStage.setTitle("Pluton Project");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 325));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
