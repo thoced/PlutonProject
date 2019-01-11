82a1467dbacb24feebb74de38b972881d5fcbead
Geoffrey@Vickx.be <3529oPtion>
2019-01-11 22:17:07 +0100

    Ajout des TreeView (Section et Dossier)

--- src/controllers/MainFormCtrl.java
+++ src/controllers/MainFormCtrl.java
@@ -1,27 +1,54 @@
 package controllers;
 
+import dao.DAOFactory;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import models.DossierModel;
+import models.SectionModel;
 
+import javax.swing.event.ChangeEvent;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
 
-public class MainFormCtrl implements Initializable,PlutonControllers {
+public class MainFormCtrl implements PlutonController, Initializable, ChangeListener<TreeItem<SectionModel>> {
 
     private Stage stage;
 
+    private  TreeItem root;
+    private TreeItem rootCase;
+
+    @FXML
+    TreeView sectionTreeView;
+    @FXML
+    TreeView casesTreeView;
+
+    @FXML
+    SplitPane splitPane;
+
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
-        if(!this.LogIn()){
+
+        /*if(!this.LogIn()){
             Platform.exit();
-        }
+        }*/
 
     }
 
@@ -50,8 +77,67 @@ public class MainFormCtrl implements Initializable,PlutonControllers {
 
     }
 
-    @Override
-    public void InitializeComponents(Stage stage) {
+    public void InitializeComponents(Stage stage) throws SQLException {
         this.stage = stage;
+
+        //region Affichage des sections
+
+        root = new TreeItem("Sections");
+        rootCase = new TreeItem("Dossiers");
+
+        this.sectionTreeView.setRoot(root);
+        this.casesTreeView.setRoot(rootCase);
+
+
+        this.sectionTreeView.getSelectionModel().selectedItemProperty().addListener(this);
+
+
+        try {
+            List<SectionModel> sections = DAOFactory.getInstance().getSectionDAO().selectAll(1,false);
+
+            for(SectionModel section:sections){
+                if(section.getId() == 1) continue;
+
+                TreeItem item = new TreeItem(section);
+                root.getChildren().add(item);
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        root.setExpanded(true);
+
+        //endregion
+
+
+    }
+
+
+    @Override
+    public void changed(ObservableValue<? extends TreeItem<SectionModel>> observable, TreeItem<SectionModel> oldValue, TreeItem<SectionModel> newValue) {
+
+        if(newValue == null)
+            return;
+
+        rootCase.getChildren().clear();
+
+        if(newValue == root)
+            return;
+
+        try {
+            List<DossierModel> dossiers = DAOFactory.getInstance().getDossierDAO().selectFromForeignKey(newValue.getValue().getId());
+
+            for(DossierModel model : dossiers){
+                TreeItem<DossierModel> itemDossier = new TreeItem<>();
+                itemDossier.setValue(model);
+                rootCase.getChildren().add(itemDossier);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
